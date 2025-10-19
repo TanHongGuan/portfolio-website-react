e3c84620b9555fb2bda2837fe24d20c6f21a65df
Yuan <yuan291104@gmail.com>
2025-10-19 19:39:23 +0800

    Created invididual css section

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import Header from "./sections/header.jsx"
 import Contact from "./sections/Contact.jsx"
-import "./App.css"
-
+import "./sections/Contact.css"
+import "./sections/Header.css"
 export default function App() {
   return (
     <div id="HomePage">
