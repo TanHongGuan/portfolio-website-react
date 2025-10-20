f40010df6c6f657ca9d0a393fa2b2ae119e269b7
Yuan <yuan291104@gmail.com>
2025-10-20 15:00:26 +0800

    Imported section with aboutme+resume+projects

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import Header from "./sections/header.jsx"
 import Contact from "./sections/Contact.jsx"
+import AboutMe_Project_Resume from "./sections/AboutMe-Project-Resume/AboutMe-Project.jsx"
+import "./sections/AboutMe-Project-Resume/AboutMe-Project.css"
 import "./sections/Contact.css"
 import "./sections/Header.css"
+
 export default function App() {
   return (
     <div id="HomePage">
       <Header />
       <Contact />
+      <AboutMe_Project_Resume />
     </div>
   );
 }
